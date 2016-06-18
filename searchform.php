da23138b0c56ab3691d1f4640a6349e7c65af3c9
Thomas W <netzberufler@googlemail.com>
2016-06-18 20:58:07 +0200

    improved coding of content template files and searchform

--- searchform.php
+++ searchform.php
@@ -9,10 +9,14 @@
 
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
-		<span class="screen-reader-text"><?php _ex( 'Search for:', 'label', 'pocono' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'pocono' ); ?>" value="<?php echo esc_attr( get_search_query() ); ?>" name="s">
+		<span class="screen-reader-text"><?php echo esc_html_x( 'Search for:', 'label', 'pocono' ); ?></span>
+		<input type="search" class="search-field"
+			placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'pocono' ); ?>"
+			value="<?php echo esc_html( get_search_query() ); ?>" name="s"
+			title="<?php echo esc_attr_x( 'Search for:', 'label', 'pocono' ); ?>" />
 	</label>
 	<button type="submit" class="search-submit">
 		<span class="genericon-search"></span>
+		<span class="screen-reader-text"><?php echo esc_html_x( 'Search', 'submit button', 'pocono' ); ?></span>
 	</button>
 </form>
