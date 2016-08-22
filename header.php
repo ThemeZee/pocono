edffe050aba4ad4d8e9822171a9485ca25219568
Thomas W <netzberufler@googlemail.com>
2016-08-22 14:59:17 +0200

    added gulp workflow

--- header.php
+++ header.php
@@ -22,24 +22,24 @@
 <body <?php body_class(); ?>>
 
 	<div id="page" class="hfeed site">
-		
+
 		<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'pocono' ); ?></a>
-		
+
 		<div id="header-top" class="header-bar-wrap"><?php do_action( 'pocono_header_bar' ); ?></div>
-		
+
 		<header id="masthead" class="site-header clearfix" role="banner">
-			
+
 			<div class="header-main container clearfix">
-				
+
 				<button id="main-navigation-toggle" class="main-navigation-toggle"></button>
-				
+
 				<div id="logo" class="site-branding clearfix">
-				
+
 					<?php pocono_site_logo(); ?>
 					<?php pocono_site_title(); ?>
-				
+
 				</div><!-- .site-branding -->
-				
+
 				<div id="header-social-icons" class="header-social-icons social-icons-navigation clearfix">
 					<?php
 						wp_nav_menu( array(
@@ -55,13 +55,11 @@
 						);
 					?>
 				</div>
-				
-
 
 			</div><!-- .header-main -->
-		
+
 		</header><!-- #masthead -->
-		
+
 		<?php pocono_header_image(); ?>
-		
+
 		<div id="content" class="site-content container clearfix">
