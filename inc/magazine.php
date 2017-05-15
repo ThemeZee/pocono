5355c6bae01667e1acb2a86a0536bad6d832caf5
Lenovo-PC\thomasw <kontakt@themecoder.de>
2017-05-15 18:39:55 +0200

    disable transient post caching in Customizer

--- inc/magazine.php
+++ inc/magazine.php
@@ -73,7 +73,7 @@ function pocono_get_magazine_post_ids( $cache_id, $category, $number_of_posts ) 
 	$cache_id = sanitize_key( $cache_id );
 	$post_ids = get_transient( 'pocono_magazine_post_ids' );
 
-	if ( ! isset( $post_ids[ $cache_id ] ) ) {
+	if ( ! isset( $post_ids[ $cache_id ] ) || is_customize_preview() ) {
 
 		// Get Posts from Database.
 		$query_arguments = array(
